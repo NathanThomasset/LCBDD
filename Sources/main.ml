1675f89d93be8b79adf237b6adcdcda63c0c2bd0
Nathan Thomasset <nathan.thomasset@ens-rennes.fr>
2016-05-06 21:57:34 +0200

    Build

--- Sources/main.ml
+++ Sources/main.ml
@@ -27,9 +27,25 @@ let u = add t 1 0 1 in
     print_t t v "bla.dot";;
 
 (* creates, if necessary, a node with variable i, low l and high h in tableT tablet as well as tableH tableh
-  returns the new node index*)
+  returns the new node index *)
 let make tablet tableh i l h =
   if (member tableh i l h) then lookup tableh i l h
   else let u = add tablet i l h in
     insert tableh i l h u;
     u;;
+
+(* creates in tableT tablet and tableH tableh the ROBDD corresponding to the given formula *)
+let rec build tablet tableh formula =
+  match formula with
+    False -> zero
+    |True -> one
+    |Atom(P(i)) -> let u = make tablet tableh i zero one in u
+    |Not(f) -> let u = build tablet tableh f in apply_neg tablet tableh u
+    |And(f,g) -> let u = build tablet tableh f and v = build tablet tableh g
+                 in apply tablet tableh Et u v
+    |Or(f,g) -> let u = build tablet tableh f and v = build tablet tableh g
+                in apply tablet tableh Ou u v
+    |Imp(f,g) -> let u = build tablet tableh f and v = build tablet tableh g
+                 in apply tablet tableh Impl u v
+    |Iff(f,g) -> let u = build tablet tableh f and v = build tablet tableh g
+                 in apply tablet tableh Equiv u v;;
